bc95e3c70c4389a5d18523df1ab100c182a3526f
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2023-10-02 20:42:05 +0530

    created function to send data to node servers

--- the-log-book/src/App.js
+++ the-log-book/src/App.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [form, setForm] = useState({});
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   const handleForm = (e) => {
     setForm({
@@ -25,20 +29,57 @@ function App() {
     //  It is a async function so we have to add await infront of it
     const data = await response.json();
 
-    console.log(data);
+    getUsers();
   };
+
+  const getUsers = async () => {
+    const response = await fetch("http://localhost:8080/demo", {
+      method: "GET",
+    });
+    // the data coming from fetch needs to be processed either By .text() pr by .json()
+    //  It is a async function so we have to add await infront of it
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  const ListElement = ({ user }) => {
+    return (
+      <div className="elementContainer">
+        <p>{user.username} </p>
+        <p>{user.password}</p>
+      </div>
+    );
+  };
+  const ListContainer = ({ users }) => {
+    return (
+      <div className="listContainer">
+        <div className="heading">
+          <h3>Username </h3>
+          <h3>Password</h3>
+        </div>
+        {users.map((user) => {
+          return <ListElement user={user} key={user._id} />;
+        })}
+      </div>
+    );
+  };
+
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        {/* this is Used to debug */}
-        {/* <p>{JSON.stringify(form) }</p> */}
-        <h3>Username</h3>
-        <input type="text" name="username" onChange={handleForm}></input>
-        <h3>Password</h3>
-        <input type="password" name="password" onChange={handleForm}></input>
-        <input type="submit"></input>
-      </form>
-    </div>
+    <>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          {/* this is Used to debug */}
+          {/* <p>{JSON.stringify(form) }</p> */}
+          <h>Username</h>
+          <input type="text" name="username" onChange={handleForm}></input>
+          <p>Password</p>
+          <input type="password" name="password" onChange={handleForm}></input>
+          <input type="submit"></input>
+        </form>
+      </div>
+
+      <ListContainer users={users} />
+    </>
   );
 }
 
